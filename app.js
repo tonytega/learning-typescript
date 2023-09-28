c618b0758e0443db5887a1ace6262b94312a4808
tonytega <aakpovire@gmail.com>
2023-09-28 01:50:14 +0100

    learnt type aliases,union & literal type etc.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-        return result;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
-        return result;
     }
+    //   if(resultConversion === 'as-number'){
+    //     return +result
+    //   }else{
+    //   return result.toString();
+    //   }
+    return result;
 }
-var combinedAges = combine(1, 32);
+var combinedAges = combine(1, 32, "as-number");
 console.log(combinedAges);
-var combinedNames = combine('Zoe ', 'Saldana');
+var combinedStringAges = combine("1", "32", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Zoe ", "Saldana", "as-text");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,16 +1,28 @@
-function combine(input1:number | string,input2:number | string){
-    let result
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-     result = input1 + input2
-     return result
-    }else{
-        result = input1.toString() + input2.toString()
-    return result;
-}
-    
+type unionType = number | string;
+type literalType = 'as-number' | 'as-text'
+
+function combine(
+  input1: unionType,
+  input2: unionType,
+  resultConversion: literalType
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+//   if(resultConversion === 'as-number'){
+//     return +result
+//   }else{
+//   return result.toString();
+//   }
+return result
 }
 
-const combinedAges = combine(1,32)
-console.log(combinedAges)
-const combinedNames = combine('Zoe ','Saldana')
-console.log(combinedNames)
+const combinedAges = combine(1, 32, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("1", "32", "as-number");
+console.log(combinedStringAges);
+const combinedNames = combine("Zoe ", "Saldana", "as-text");
+console.log(combinedNames);
